236f40d986cde28606426fbe312da120311c73d9
EnglishBeach <kraynoff.i@yandex.ru>
2022-12-03 07:58:18 +0000

    add parametrs module

---
+++ test/base.py
@@ -0,0 +1,15 @@
+def foo(a, b):
+    return a + b
+
+
+L = 102
+
+
+class Base_class:
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def method(self):
+        print(self.x, self.y)
